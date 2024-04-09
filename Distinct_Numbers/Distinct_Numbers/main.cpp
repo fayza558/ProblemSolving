929fc038f483d8d1ce6c400bc3b757d8d5d96716
fayza mahmoud <110182701+fayza558@users.noreply.github.com>
2024-04-09 22:02:41 +0200

    Add files via upload

---
+++ Distinct_Numbers/Distinct_Numbers/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    set<int> distinctValues;
+
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        distinctValues.insert(x);
+    }
+
+    cout << distinctValues.size() << endl;
+
+    return 0;
+}
+
+
