7912770c8b6d0c58862c33f39bde31a7f58bdd83
fayza mahmoud <110182701+fayza558@users.noreply.github.com>
2024-04-27 03:23:42 +0300

    Add files via upload

--- A_Raising_Bacteria/A_Raising_Bacteria/main.cpp
+++ A_Raising_Bacteria/A_Raising_Bacteria/main.cpp
@@ -18,3 +18,39 @@ int main()
    cout<<cntr<<endl;
     return 0;
 }
+
+  //anothor solution with bitmask
+
+  #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int cntr=0;
+   while(n>0)
+   {
+       if(n&1)
+       {
+           cntr++;
+       }
+       n>>=1;
+   }
+   cout<<cntr<<endl;
+    return 0;
+}
+
+ //anothor solution with bitmask function that count the number of bits that is on
+  #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<__builtin_popcount(n)<<endl;
+    return 0;
+}
