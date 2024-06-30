aad0f3fc08718357df379372b0f5d3659311639b
fayza mahmoud <110182701+fayza558@users.noreply.github.com>
2024-06-30 16:46:08 +0300

    Add files via upload

---
+++ B. Indivisible/B. Indivisible/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n==1)
+        {
+            cout<<1<<endl;
+        }else if(n&1)
+        {
+             cout<<-1<<endl;
+        }
+        else
+        {
+            for(int i=1;i<=n;i+=2)
+            {
+                cout<<i+1<<" "<<i<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
