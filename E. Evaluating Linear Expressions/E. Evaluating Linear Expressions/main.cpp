d77d9f3ef27948d9e6659fe27492d8e3ad5c2227
fayza mahmoud <110182701+fayza558@users.noreply.github.com>
2024-07-01 21:57:39 +0300

    Add files via upload

---
+++ E. Evaluating Linear Expressions/E. Evaluating Linear Expressions/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,k;
+    cin>>a>>b>>k;
+    for(int i=1;i<=k;i++)
+    {
+        cout<<a*i+b<<' ';
+    }
+    return 0;
+}
